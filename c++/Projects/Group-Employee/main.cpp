0a2a0dc724951741be7bd1a778ecf586808d357f
Hao Wei <65269850+Howw-Way@users.noreply.github.com>
2022-06-28 12:37:36 +0800

    Add files via upload

---
+++ c++/Projects/Group-Employee/main.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+#include<vector>
+#include<map>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class Employee {
+public:
+	Employee(string name,int salary) {
+		m_name = name;
+		m_salary = salary;
+	}
+	string m_name;
+	int m_salary;
+
+};
+
+void ShowEmployee(vector<Employee> &v) {
+	for (vector<Employee>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << it->m_name <<" "<<"Salary:" << it->m_salary << endl;
+	}
+}
+
+void CreatEmployee(vector<Employee> &v) {
+	string employee_name = "ABCDEFGHIJ";
+	for (int i = 0; i < 10; i++)
+	{
+		int salary = rand() % 10000 + 10000;
+		string em_name= "Employee: ";
+		em_name += employee_name[i];
+		Employee e1(em_name, salary);
+		v.push_back(e1);
+	}
+}
+
+void SetGroup(vector<Employee>& v,multimap<int,Employee>& mm) {
+	for (int i = 0; i < 10; i++)
+	{
+		int dep_id = rand() % 3;
+		mm.insert(make_pair(dep_id, v[i]));
+	}
+}
+
+void ShowGroup(multimap<int, Employee>& mm) {
+	for (int i = 0; i < 3; i++)
+	{
+		multimap<int, Employee>::iterator pos = mm.find(i);
+		int count = mm.count(i);
+		int index = 0;
+		for (; pos != mm.end() && index < count; pos++, index++) {
+			cout << "Depart " << (*pos).first << ":";
+			cout << (*pos).second.m_name << " " << "Salary:" << (*pos).second.m_salary << endl;
+		}
+	}
+
+}
+
+int main() {
+	vector<Employee> v;
+	CreatEmployee(v);
+	multimap<int, Employee> mm;
+	SetGroup(v, mm);
+	ShowGroup(mm);
+	//ShowEmployee(v);
+	return 0;
+}
